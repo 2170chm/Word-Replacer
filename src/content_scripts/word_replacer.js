86585625fdb8418920c40c8122a705ee277e70b0
Sivan Cooperman <sivancooperman@gmail.com>
2023-02-11 21:35:49 -0500

    Implemented replace function logic
    Implemented the replace() function which replaces a word with a user-given word. Also made minor changes to the popup files to work with the function.

--- src/content_scripts/word_replacer.js
+++ src/content_scripts/word_replacer.js
@@ -1,3 +1,6 @@
+// Parts of this file were copied from Mozilla's "Your Second Extension"
+// article, found at https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Your_second_WebExtension
+
 (() => {
     /**
      * Global variable to prevent the script from being injected in the same page again.
@@ -10,15 +13,25 @@
     /**
      * Replace all occurrences of the first parameter 'originalWord' with
      * the second parameter 'newWord' in the current html
+     *
+     * The logic for replacing in-text instances of a word within a HTML body
+     * was drawn from answers at https://stackoverflow.com/questions/5558613/replace-words-in-the-body-text
      */
-    function replace(originalWord, newWord) {
-      /* TODO: Traverse the DOM structure of the html and use replaceAll() to replace all occurrences of originalWord with newWord */
+    function replace(element, originalWord, newWord) {
+        if (element.childNodes.length) {
+            element.childNodes.forEach((child) => replace(child, originalWord, newWord));
+        } else {
+            const text = element.textContent;
+            if (text) {
+                element.textContent = text.replace(new RegExp(originalWord,"g"), newWord);
+            }
+        }
     }
   
     /**
      * Listen for messages, upon receiving one, run replace()
      */
     browser.runtime.onMessage.addListener((message) => {
-      replace(message.originalWord, message.replaceWith)
+        replace(document.body, message.originalWord, message.replaceWith);
     });
   })();
